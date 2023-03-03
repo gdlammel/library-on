b904ba4b4233fd145d15185ca20b48904c1d8a45
Guilherme Duarte <gdlammel04@gmail.com>
2023-03-03 00:25:33 -0300

    Change default null in some book model fields

---
+++ prisma/migrations/20230303032301_add_default_null_in_some_fields_book_table/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Book" ALTER COLUMN "details" DROP NOT NULL,
+ALTER COLUMN "publishYear" DROP NOT NULL,
+ALTER COLUMN "publishingCompany" DROP NOT NULL,
+ALTER COLUMN "quantityInStock" SET DEFAULT 0;
